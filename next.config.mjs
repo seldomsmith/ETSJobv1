f687bc6ffc29132ad8b901da46cd9f82a0bdf774
Matthew Dow <matthew.dow@edmonton.ca>
2026-03-14 20:52:17 -0600

    fix: patch next.js version and disable optimizeFonts for codespace

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
+    optimizeFonts: false, // Disables static font fetching which can fail in secure Codespaces
 };
 
 export default nextConfig;
+
